c226ca72f4258af3da16ee68bc53ad01ad226b1e
Collin Drake <collinlindendrake@gmail.com>
2022-12-15 17:53:35 -0700

    //

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use ndarray::Array2;
+
+fn main () {
+    println!("hi")
+}
+
+#[derive(Debug)]
+struct Network {
+    num_layers: usize,
+    sizes: Vec<usize>,
+    biases: Vec<Array2<f64>>,
+    weights: Vec<Array2<f64>>,
+}
+
+use rand::distributions::StandardNormal;
+use ndarray::{Array, Array2};
+use ndarray_rand::rand_distr::StandardNormal;
+use ndarray_rand::RandomExt;
+
+impl Network {
+    fn new(sizes: &[usize]) -> Network {
+        let num_layers = sizes.len();
+        let mut biases: Vec<Array2<f64>> = Vec::new();
+        let mut weights: Vec<Array2<f64>> = Vec::new();
+        for i in 1..num_layers {
+            biases.push(Array::random((sizes[i], 1), StandardNormal));
+            weights.push(Array::random((sizes[i], sizes[i - 1]), StandardNormal));
+        }
+        Network {
+            num_layers: num_layers,
+            sizes: sizes.to_owned(),
+            biases: biases,
+            weights: weights,
+        }
+    }
+}
