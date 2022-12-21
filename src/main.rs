488fd4becc695141049e7f92a0ee5786d9cc3778
Collin Drake <collinlindendrake@gmail.com>
2022-12-21 13:12:29 -0700

    //

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main () {
     let mut arr: [i32; 4] = [4, 6, 8, 10];
 
     for x in 0..arr.len() {
-        arr[x] = x.pow(x as u32) as i32;
+        arr[x] = arr[x].pow(x as u32) as i32;
         println!("x: {}", arr[x]);
     }
 }
