2788504e3cae7ecea87b7aae8adb09f1981bc9a0
Collin Drake <collinlindendrake@gmail.com>
2022-12-21 13:36:39 -0700

    //

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use std::cmp::max;
+
 mod new_in_rust;
 mod cnn;
 
 fn main () {
     let mut arr: [i32; 4] = [4, 6, 8, 10];
 
-    for x in 0..arr.len() {
-        arr[x] = arr[x].pow(x as u32) as i32;
-        println!("x: {}", arr[x]);
+    for num in 0..arr.len() {
+        arr[num] = re_lu(arr[num]);
+        println!("{}: {}", num, arr[num]);
+    }
+
+    fn re_lu(x: i32) -> i32 {
+        max(0,x)
+    }
+
+    fn tanh(x: i32) -> i32 {
+        x
     }
 }
