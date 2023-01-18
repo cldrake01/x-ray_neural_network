fc0b12516f87b149a51b4fba95dbf55a20dca77d
Collin Drake <collinlindendrake@gmail.com>
2023-01-18 00:32:56 -0700

    //

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use std::cmp::max;
+mod songStruct;
 
-mod new_in_rust;
-mod cnn;
+use std::fs::File;
+use std::io::BufReader;
+use std::time::Duration;
+use rodio::{Decoder, OutputStream, Sink};
+use rodio::source::{SineWave, Source};
 
 fn main () {
-    let mut arr: [i32; 4] = [4, 6, 8, 10];
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
 
-    for num in 0..arr.len() {
-        arr[num] = re_lu(arr[num]);
-        println!("{}: {}", num, arr[num]);
-    }
+// Add a dummy source of the sake of the example.
+    let file = BufReader::new(File::open("/Users/collindrake/Downloads/Airborne_Grooves.wav").unwrap());
+// Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    sink.append(source);
 
-    fn re_lu(x: i32) -> i32 {
-        max(0,x)
-    }
-
-    fn tanh(x: i32) -> i32 {
-        x
-    }
+// The sound plays in a separate thread. This call will block the current thread until the sink
+// has finished playing all its queued sounds.
+    sink.sleep_until_end();
 }
---
+++ src/songStruct.rs
@@ -0,0 +1,34 @@
+pub struct Song {
+    plays: i32,
+    name: String,
+    path: String
+}
+
+pub struct Playlist {
+    name: String,
+    songs: Vec<Song>
+}
+
+impl Song {
+    pub fn set(&mut self, plays: i32, name: String, path: String) {
+        self.plays = plays;
+        self.name = name;
+        self.path = path;
+    }
+
+    pub fn get(&self) -> (i32, String, String) {
+        (self.plays, self.name.clone(), self.path.clone())
+    }
+}
+
+impl Playlist {
+    pub fn playlist(mut self, songs: &[Song]) {
+        for song in songs {
+            self.songs.push(*song);
+        }
+    }
+
+    pub fn add(&mut self, new_song: Song) {
+        self.songs.push(new_song);
+    }
+}
